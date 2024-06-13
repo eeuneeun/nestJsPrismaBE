b9c136b4fe431e04a7f8cc54bf2143bc952f9077
euneun <sacroo@naver.com>
2024-06-13 17:46:15 +0900

    소켓 게이트웨이 연결 & 메시지 로그 확인 완료

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -20,11 +20,13 @@ import {
   
     @SubscribeMessage('events')
     findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+      console.log(data)
       return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
     }
   
     @SubscribeMessage('identity')
     async identity(@MessageBody() data: number): Promise<number> {
+      console.log(data)
       return data;
     }
   }
